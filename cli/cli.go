1322d12a8da382297ee2fcee4be77c6afa976fb9
LazyCode2 <21darkwarrior@gmail.com>
2026-02-12 14:18:16 +0600

    feat: add more flags for documenting APIs

--- cli/cli.go
+++ cli/cli.go
@@ -5,7 +5,12 @@ import (
 )
 
 var (
-	addEndpoint = flag.String("add", "", "API endpoint to add (e.g., api/v1/homepage)")
+	addEndpoint  = flag.String("add", "", "API endpoint to add (e.g., api/v1/homepage)")
+	method       = flag.String("m", "GET", "HTTP method (GET, POST, PUT, DELETE, PATCH)")
+	description  = flag.String("d", "", "Endpoint description")
+	requestBody  = flag.String("body", "", "Request body schema (optional)")
+	responseBody = flag.String("response", "", "Response body schema (optional)")
+	statusCode   = flag.Int("status", 200, "Default status code")
 )
 
 func InitCli() {
